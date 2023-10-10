de7dc36ce9dcbfded5c810ffb430023fbb3e4b94
Max Zangs <maximilian.zangs@gmail.com>
2023-10-10 13:08:21 +0000

    setting up dev container

--- src-flask/gerfficient/__init__.py
+++ src-flask/gerfficient/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 
+
 def make_app(secret_key: str) -> Flask:
     app = Flask(__name__)
-    
+
     app.secret_key = secret_key
+
+    @app.route('/')
+    def index() -> str:
+        return "Hello, world!"
 
     return app
