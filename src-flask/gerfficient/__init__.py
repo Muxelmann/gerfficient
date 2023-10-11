6415c6f96b037c99190dcac419b1eab5f3e2dc85
Max Zangs <maximilian.zangs@gmail.com>
2023-10-11 06:07:15 +0000

    Placeholder

--- src-flask/gerfficient/__init__.py
+++ src-flask/gerfficient/__init__.py
@@ -1,13 +1,16 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 def make_app(secret_key: str) -> Flask:
     app = Flask(__name__)
 
     app.secret_key = secret_key
 
-    @app.route('/')
-    def index() -> str:
-        return "Hello, world!"
+    @app.route('/posts/')
+    def get_post_list() -> str:
+        return render_template('posts/list.jinja2')
+    
+    @app.route('/posts/sample')
+    def get_post() -> str:
+        return render_template('posts/single.jinja2')
 
     return app
