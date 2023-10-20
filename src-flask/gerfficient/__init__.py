72d832da86c903144df0618a1869aed6b42c66dc
Max Zangs <maximilian.zangs@gmail.com>
2023-10-20 15:30:55 +0000

    making app package

--- src-flask/gerfficient/__init__.py
+++ src-flask/gerfficient/__init__.py
@@ -1,16 +1,1 @@
-from flask import Flask, redirect, url_for
-
-from . import posts
-
-def make_app(secret_key: str) -> Flask:
-    app = Flask(__name__)
-
-    app.secret_key = secret_key
-
-    posts.init(app)
-
-    @app.route('/')
-    def index() -> str:
-        return redirect(url_for('posts.index'))
-
-    return app
+from .app import make_app
---
+++ src-flask/gerfficient/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for
+
+from . import posts
+
+def make_app(secret_key: str) -> Flask:
+    app = Flask(__name__)
+
+    app.secret_key = secret_key
+
+    posts.make_bp(app)
+
+    @app.route('/')
+    def index() -> str:
+        return redirect(url_for('posts.index'))
+
+    return app
